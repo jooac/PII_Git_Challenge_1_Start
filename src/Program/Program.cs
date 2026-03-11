a9921ef02a968fe22b8e13e6b66519718f90911b
jooac <joacosan2212@gmail.com>
2026-03-11 20:24:04 -0300

    version final

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,10 +2,10 @@
 {
     public static void Main()
     {
-        Console.WriteLine(Suma.Sumar(1, 2));
+        Console.WriteLine(Addition.Add(1, 2));
         Console.WriteLine(Resta.Restar(3, 4));
         Console.WriteLine(Multiplicacion.Multiplicar(5, 6));
-        Console.WriteLine(Division.Dividir(7, 8));
+        Console.WriteLine(Division.Divide(7, 8));
     }
 }
 
