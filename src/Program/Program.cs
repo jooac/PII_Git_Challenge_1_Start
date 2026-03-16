9d76f41c1cd08b58a4e0ef0309e83de3c3877390
15tizzi <santino.tizzi@correo.ucu.edu.uy>
2026-03-16 19:36:43 -0300

    Mensaje en la calculadora

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -17,3 +17,15 @@ public class Division
        return (double)a / b;
    }
 }
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Demo calculadora");
+        Console.WriteLine(Suma.Sumar(1, 2));
+        Console.WriteLine(Resta.Restar(3, 4));
+        Console.WriteLine(Multiplicacion.Multiplicar(5, 6));
+        Console.WriteLine(Division.Dividir(7, 8));
+    }
+}
